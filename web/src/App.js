55ce4d791476557c78c9bc45826799885e12ddbe
codeeverydayyu <549117762@qq.com>
2023-03-13 00:43:40 -0700

    Seperate App.js to HomePage and RecordPage.

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RecordPage from './components/RecordPage';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <main>
+          <Routes>
+            <Route path='/record' element={<RecordPage />} />
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ web/src/components/RecordPage.js
@@ -0,0 +1,7 @@
+export default function RecordPage() {
+  return (
+    <div>
+      <h1>This is Record Page</h1>
+    </div>
+  );
+}
