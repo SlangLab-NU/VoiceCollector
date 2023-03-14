fac7110bc0246ed33cd758fa2401fd04c371f072
Guoao Wei <guoao.wei@outlook.com>
2023-03-14 10:43:42 -0700

    Add a default router

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -7,11 +7,12 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-
+from .default import blueprint as default
 from .speak import blueprint as speak_blueprint
 from .validate import blueprint as validate_blueprint
 from .intel import blueprint as intelligibility_blueprint
 
+api.register_blueprint(default)
 api.register_blueprint(speak_blueprint)
 api.register_blueprint(validate_blueprint)
 api.register_blueprint(intelligibility_blueprint)
