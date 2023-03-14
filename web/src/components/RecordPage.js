6670bd27211c0cbf5cb2499e4ad429d5e44f7075
codeeverydayyu <549117762@qq.com>
2023-03-14 00:26:22 -0700

    Skip button works for text prompt. Modify data.js. Add image.js and two png files.

--- web/src/components/RecordPage.js
+++ web/src/components/RecordPage.js
@@ -1,24 +1,33 @@
+import React from 'react';
 import data from '../data.js';
+import image from '../image.js';
+import Figure1 from '../Figure-1.png';
 
 export default function RecordPage() {
-  let isPromptText = true;
-  let i = 4;
-  let item = data[i];
-  let currentPromptNumber = item.promptNum;
-  const totalPromptNumber = data.length;
-  let currentPromptSectionName = item.section;
-  let prompt = item.prompt;
-  let isRecorded = false;
+  const [isPromptText, setIsPromptText] = React.useState(true);
+  const initialPromptNum = 0;
+  const [item, setItem] = React.useState(data[initialPromptNum]);
+  const isRecorded = false;
+  const [imagePath, setImagePath] = React.useState('');
+  const totalPromptNum = data.length + image.length;
+
+  function handleSkip() {
+    if (item.promptNum < data.length - 1) {
+      setItem((prevItem) => {
+        return data[prevItem.promptNum + 1];
+      });
+    } else {
+      setIsPromptText((prevIsText) => !prevIsText);
+    }
+  }
 
   return (
     <div className='record-page-container'>
       <div className='prompt-info-container'>
         <p className='propmt-sequential-number'>
-          Prompt Number: {currentPromptNumber}/{totalPromptNumber}
+          Prompt Number: {item.promptNum + 1}/{totalPromptNum}
         </p>
-        <p className='prompt-section-name'>
-          Section: {currentPromptSectionName}
-        </p>
+        <p className='prompt-section-name'>Section: {item.section}</p>
       </div>
       <div className='prompt-and-button-container'>
         {isPromptText ? (
@@ -26,10 +35,13 @@ export default function RecordPage() {
             className='prompt-text-parent'
             style={{ height: '300px', overflowY: 'scroll' }}
           >
-            <p className='prompt-text'>{prompt}</p>
+            <p className='prompt-text'>{item.prompt}</p>
           </div>
         ) : (
-          <div>Image goes here</div>
+          <div>
+            <p>Tell me what is happening in this picture</p>
+            <img src={Figure1} alt='kitchen' className='image' />
+          </div>
         )}
         <div className='prompt-button'>
           <button id='play' disabled={!isRecorded}>
@@ -43,7 +55,9 @@ export default function RecordPage() {
       <div className='button-container'>
         <button id='shortcuts'>Shortcuts</button>
         <button id='record'>Record</button>
-        <button id='skip'>Skip</button>
+        <button id='skip' onClick={handleSkip}>
+          Skip
+        </button>
         <button id='submit'>Submit</button>
       </div>
     </div>
---
+++ web/src/image.js
@@ -0,0 +1,14 @@
+const image = [
+  {
+    promptNum: 5,
+    section: 'Image',
+    imageName: 'Figure-1.png',
+  },
+  {
+    promptNum: 6,
+    section: 'Image',
+    imageName: 'Figure-2.png',
+  },
+];
+
+export default image;
