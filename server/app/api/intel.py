d043abe7a06cf4187fcc3022ad0d47a4cd243a79
Guoao Wei <guoao.wei@outlook.com>
2023-03-21 11:30:19 -0700

    Add handler for `get_scores`

--- server/app/api/intel.py
+++ server/app/api/intel.py
@@ -2,38 +2,26 @@
 Intel API route handlers. It handles requests to calculate the intelligibility score.
 
 """
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from ..scripts import intel_score
 
 blueprint = Blueprint('intel', __name__, url_prefix="/intel")
 
-class Model:
-    def __init__(self) -> None:
-        pass
-    
-    def __del__(self):
-        pass
-    
-    def inference(self):
-        pass
-
-model = Model()
-
-@blueprint.route('/')
+@blueprint.route('/', methods=["GET"])
 def get_scores():
-    model.inference()
-    return jsonify(dict(msg="/get_scores"))
-
+    # After db is setup, `data` should look like `{"audio_id", "..." "ref_id": "..."}` and the audio file is uploaded
+    data = request.json
+    y_pred, y_true = data["y_pred"], data["y_true"]
+    return intel_score.evaluate(y_pred, y_true)
+    
 
 @blueprint.route('/sequence_matcher')
 def get_sequence_matcher_score():
-    model.inference()
-
     return jsonify(dict(msg="/sequence_matcher"))
 
 
-@blueprint.route('/levenshtein_distance')
-def get_levenshtein_distance_score():
-    model.inference()
-    return jsonify(dict(msg="/levenshtein_distance"))
+# @blueprint.route('/levenshtein_distance')
+# def get_levenshtein_distance_score():
+#     return jsonify(dict(msg="/levenshtein_distance"))
 
 
