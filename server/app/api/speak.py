e05596297c4bec4e3cdbe1b3bb700f8df9b9aaae
codeeverydayyu <549117762@qq.com>
2023-03-22 00:38:15 -0700

    Add version number to requirements. Move get_data to speak.py but have error.

--- server/app/api/speak.py
+++ server/app/api/speak.py
@@ -3,6 +3,7 @@ Speak API route handlers. They handle requests related to reference text, record
 
 """
 from flask import Blueprint, jsonify
+from .. import conn 
 
 blueprint = Blueprint('speak', __name__, url_prefix="/speak")
 
@@ -27,4 +28,10 @@ def get_reference():
     Returns:
         _type_: _description_
     """
-    return jsonify(dict(texts=["The cat sat on the mat.", ]))
+    with conn.cursor() as cursor:
+        # cursor.execute('SELECT * FROM reference')
+        cursor.execute('use voicecollector')
+        cursor.execute('select * from reference')
+        result = cursor.fetchall()
+        return jsonify(result)
+    #return jsonify(dict(texts=["The cat sat on the mat.", ]))
