7bb043ea1006a3b0f6d90c23e5a8537cdbdac6bf
codeeverydayyu <549117762@qq.com>
2023-03-23 00:27:14 -0700

    Add tests of /get_reference api and reference schema.

--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -3,7 +3,7 @@ Tests for API handlers
 
 """
 import json
-
+import jsonschema
 import pytest
 
 from app import create_app
@@ -35,3 +35,36 @@ def test_not_found(client):
 #     assert response.status_code == 200
 #     assert parsed_data.get('hello') == 'foo'
 # TODO: add tests for all routers
+
+def test_get_reference_api(client):
+    response = client.get('/api/v1/speak/get_reference')
+    print(response.status_code)
+    assert response.status_code == 200
+
+
+# Define the reference schema as a JSON object
+REFERENCE_SCHEMA = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "image_url": {"type": ["null", "string"]},
+            "ref_id": {"type": "number"},
+            "section": {"type": "string"},
+            "text": {"type": "string"}
+        },
+        "required": ["ref_id", "section", "text"]
+    }
+}
+
+def test_validate_json_schema(client):
+    # Define the JSON data to be validated
+    response = client.get('/api/v1/speak/get_reference')
+    parsed_data = json.loads(response.data)
+    # Validate the JSON data against the schema
+    jsonschema.validate(instance=parsed_data, schema=REFERENCE_SCHEMA)
+    # If the JSON data is valid against the schema, the test passes
+    assert True
+
+
+    
