d9d90f2d9b8d7da87942afac22625143e1c1e213
Yuhan Yue <yuhan0810@outlook.com>
2023-03-29 15:36:26 -0700

    remove secret key

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -11,7 +11,6 @@ from .errors import add_error_handlers
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     CORS(app, resources={r'/*': {'origins': '*'}})
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
     add_error_handlers(app)
