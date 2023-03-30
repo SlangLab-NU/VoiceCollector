4a51523de6be2bf6abc7a0cffa469b7a1d1dfd12
Guoao Wei <guoao.wei@outlook.com>
2023-03-29 22:31:17 -0700

    Add test cases and samples for format check

--- server/tests/test_validate.py
+++ server/tests/test_validate.py
@@ -10,6 +10,23 @@ def client():
     app = create_app()
     return app.test_client()
 
+def test_format_check_pass(client):
+    samples_dir = pathlib.Path(__file__).parent.resolve() / "samples"
+    filenames = ["silence_valid_format.wav", "silence_stereo.wav", "silence_22khz.wav", "silence.m4a"]
+    results = [True, False, False, False]
+
+    for fname, result in zip(filenames, results):
+        audio_file = samples_dir / fname
+        with open(audio_file, 'rb') as f:
+            data = {"file": f}
+            response = client.post(
+                "/api/v1/validate/format",
+                data=data,
+            )
+        
+        parsed_data = json.loads(response.data)
+        assert parsed_data["result"] == result
+
 
 def test_silence_check_pass(client):
     # current_dir = pathlib.Path(__file__).parent.resolve()
