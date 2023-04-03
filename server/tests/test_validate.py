40512ee4d816783c88377217430a84d82b43bd0c
Guoao Wei <guoao.wei@outlook.com>
2023-04-02 23:08:11 -0700

    Add test cases for validate

--- server/tests/test_validate.py
+++ server/tests/test_validate.py
@@ -12,8 +12,18 @@ def client():
 
 def test_format_check_pass(client):
     samples_dir = pathlib.Path(__file__).parent.resolve() / "samples"
-    filenames = ["silence_valid_format.wav", "silence_stereo.wav", "silence_22khz.wav", "silence.m4a"]
-    results = [True, False, False, False]
+    filenames = [
+        "silence_valid_format.wav",
+        "silence_stereo.wav",
+        "silence_22khz.wav",
+        "silence.m4a",
+    ]
+    results = [
+        "{'result': True}",
+        "{'channels': {'actual': 2, 'expected': 1}, 'result': False}",
+        "{'channels': {'actual': 2, 'expected': 1}, 'result': False, 'sample_rate': {'actual': 22050, 'expected': 48000}}",
+        "{'filename_format': {'actual': 'm4a', 'expected': 'wav'}, 'result': False}",
+    ]
 
     for fname, result in zip(filenames, results):
         audio_file = samples_dir / fname
@@ -23,24 +33,34 @@ def test_format_check_pass(client):
                 "/api/v1/validate/format",
                 data=data,
             )
-        
+        assert response.status_code == 200
         parsed_data = json.loads(response.data)
-        assert parsed_data["result"] == result
+        assert str(parsed_data) == result
 
 
 def test_silence_check_pass(client):
-    # current_dir = pathlib.Path(__file__).parent.resolve()
-    # audio_file = current_dir/"test.wav"
-    # data = {"file": (audio_file, "test.wav")}
-        
-    # client.post(
-    #     data=data,
-    #     buffered=True,
-    #     content_type="multipart/form-data",
-    # )
-    
-    # response = client.post('/volume_pause')
-    # assert response.status_code == 200
-    # parsed_data = json.loads(response.data)
-    response = client.post('/volume_pause', data={})
-    assert response.status_code == 200
+    samples_dir = pathlib.Path(__file__).parent.resolve() / "samples"
+    filenames = [
+        "voice_gawei.wav",
+        "low_voice_silence.wav",
+        "low_voice_long_pause.wav",
+        "low_voice_very_long_silence.wav",
+    ]
+    results = [
+        "{'result': True, 'silence_ratio': 0.3928571428571429, 'volume': -29.117730341169}",
+        "{'result': True, 'silence_ratio': 0.7074235807860262, 'volume': -41.00134167022599}",
+        "{'result': True, 'silence_ratio': 0.8668639053254438, 'volume': -42.668657847696984}",
+        "{'result': True, 'silence_ratio': 0.7418224299065421, 'volume': -41.61762061851039}"
+    ]
+
+    for fname, result in zip(filenames, results):
+        audio_file = samples_dir / fname
+        with open(audio_file, 'rb') as f:
+            data = {"file": f}
+            response = client.post(
+                "/api/v1/validate/volume_pause",
+                data=data,
+            )
+        assert response.status_code == 200
+        parsed_data = json.loads(response.data)
+        assert str(parsed_data) == result
