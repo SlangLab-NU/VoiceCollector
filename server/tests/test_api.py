618d82e2c88d13657bd24465a8db2f964698360b
Guoao Wei <guoao.wei@outlook.com>
2023-04-02 23:07:48 -0700

    Add default API test

--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -15,23 +15,15 @@ def client():
     return app.test_client()
 
 
+def test_default(client):
+    response = client.get('/api/v1/')
+    assert response.status_code == 200
+    parsed_data = json.loads(response.data)
+    assert parsed_data.get('msg') == 'Welcome to VoiceCollector backend API.'
+
+
 def test_not_found(client):
     response = client.get('/noexist')
     assert response.status_code == 404
     parsed_data = json.loads(response.data)
     assert parsed_data.get('error') == 'Not Found'
-
-
-# def test_hello_world(client):
-#     response = client.get('/api/v1/hello/world')
-#     parsed_data = json.loads(response.data)
-#     assert response.status_code == 200
-#     assert parsed_data.get('hello') == 'world'
-
-
-# def test_hello_foo(client):
-#     response = client.get('/api/v1/hello/foo')
-#     parsed_data = json.loads(response.data)
-#     assert response.status_code == 200
-#     assert parsed_data.get('hello') == 'foo'
-# TODO: add tests for all routers
