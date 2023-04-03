bfd5b07c4e5229afb83c54443a17739cbbb6f162
Guoao Wei <guoao.wei@outlook.com>
2023-04-02 23:07:06 -0700

    Round duration to 2 decimals

--- server/app/scripts/length_check.py
+++ server/app/scripts/length_check.py
@@ -7,7 +7,7 @@ def get_audio_length(f):
     frames = f.getnframes()
     rate = f.getframerate()
     duration = frames / float(rate)
-    print("duration: %f",duration)
+    print("duration (s): ", round(duration, 2))
     return duration
 
         
