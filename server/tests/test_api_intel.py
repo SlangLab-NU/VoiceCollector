e66db2075f366a7020abbb4017c34a3cb7ad0d43
Guoao Wei <guoao.wei@outlook.com>
2023-04-03 23:58:41 -0700

    Add test for intel handler

--- server/tests/test_api_intel.py
+++ server/tests/test_api_intel.py
@@ -3,8 +3,9 @@ Tests for API handlers
 
 """
 import json
-
 import pytest
+import pathlib 
+from werkzeug.datastructures import MultiDict
 
 from app import create_app
 
@@ -15,15 +16,50 @@ def client():
     return app.test_client()
 
 
-def test_get_scores(client):
-    response = client.get(
-        "/api/v1/intel/",
-        json={
-            "y_pred": "when he speaks he voices egisabe crant and quimus nus tramfl",
-            "y_true": "When he speaks, his voice is just a bit cracked and quivers a trifle.",
-        },
+def test_transcribe(client):
+    samples_dir = pathlib.Path(__file__).parent.resolve() / "samples"
+    filenames = [
+        "0174.wav",
+        "0191.wav",
+    ]
+    data = MultiDict()
+
+    for fname in filenames:
+        audio_file = samples_dir / fname
+        data.add("audio", open(audio_file, 'rb'))
+
+    response = client.post(
+        "/api/v1/intel/transcribe",
+        data=data,
     )
-    # response = client.get("/api/v1/intel")
     assert response.status_code == 200
-    parsed_data = response.json
-    assert str(parsed_data) == "{'cer': 0.5333333333333333, 'metaphone_match': 0.6428571428571428, 'sequence_matcher': 0.6875}"
+    parsed_data = json.loads(response.data)
+    assert parsed_data == ["you're used to being on the field", 'wy yell or worry over silly items']
+
+
+def test_predict_scores(client):
+    samples_dir = pathlib.Path(__file__).parent.resolve() / "samples"
+    filenames = [
+        "0174.wav",
+        "0191.wav",
+    ]
+    reference = [
+        "You're used to being on the field.",
+        "Why yell or worry over silly items?"
+    ]
+    data = MultiDict()
+
+    for fname in filenames:
+        audio_file = samples_dir / fname
+        data.add("audio", open(audio_file, 'rb'))
+    
+    for ref in reference:
+        data.add("ref", ref)
+
+    response = client.post(
+        "/api/v1/intel",
+        data=data,
+    )
+    assert response.status_code == 200
+    parsed_data = json.loads(response.data)
+    assert str(parsed_data) == '{ "cer": [ 0.9393939393939394, 0.9090909090909091 ], "metaphone_match": [ 1.0, 1.0 ], "sequence_matcher": [ 0.9850746268656716, 0.9705882352941176 ] }'
