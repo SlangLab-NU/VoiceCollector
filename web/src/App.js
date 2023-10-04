2e123d7af1b94193e7da6d86afda1fc063a9b9cc
ztybigcat <ztybigcatus@icloud.com>
2023-10-04 23:36:23 +0000

    1. Remove outdated resources; 2. Change the home page of the app to directly record. 3. Fix issues in the dev version of docker-compose

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
-import Home from './pages/Home';
-import Record from './pages/Record';
-import AudioRecorder from './components/AudioRecorderCommon';
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import RecordMUI from './pages/RecordMUI';
-import ContactUs from './components/ContactUs';
 
-
+// to add a differnt path, such as home, add a route and set component in element.
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path ="/" element={<Home />}/>
-          <Route path ="/record" element={<RecordMUI />}/>
+          <Route path ="/" element={<RecordMUI />}/>
           {/* <Route path ="/audio_recorder" element={<AudioRecorder />}/> */}
         </Routes>
       </BrowserRouter>
