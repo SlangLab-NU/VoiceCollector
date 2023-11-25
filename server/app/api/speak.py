6cd28db2bdb0b0191639aa8f025875782a1c791a
jindacz <jindaz.work@outlook.com>
2023-11-25 06:26:42 -0100

    change path

--- server/app/api/speak.py
+++ server/app/api/speak.py
@@ -101,10 +101,10 @@ def get_csv():
     Returns:
         _type_: _description_
     """
-    
+    out_dir = '/VoiceCollector/data/voice/output'
     try:
         csv_file_path = generate_csv_file()
-        return jsonify({'message': 'CSV file generation complete', 'csv_path': csv_file_path})
+        return jsonify({'message': 'CSV file generation complete', 'csv_path': out_dir})
     except Exception as e:
         return jsonify({'message': 'CSV file generation failed', 'error': str(e)})
 
